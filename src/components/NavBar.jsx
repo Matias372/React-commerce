9a3d396476d0bf696065e30c9bcc0b1d2bef32c8
Matias-Notebook <otaku372@gmail.com>
2024-12-27 12:52:35 -0300

    PreEntrega1Ruibal

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,34 +1,58 @@
-
-import CartWidget from './CartWidget';
+import CartWidget from "./CartWidget";
 
 const NavBar = () => {
-  return (
-    <nav className="navbar navbar-expand-lg bg-light fixed-top w-100 px-3">
-      <div className="container-fluid">
-        <a className="navbar-brand fs-1" href="#">La Cueva</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse justify-content-end fw-bold" id="navbarNav">
-          <ul className="navbar-nav me-4">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Inicio</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Categoría 1</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Categoría 2</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Categoría 3</a>
-            </li>
-          </ul>
-        </div>
-        <CartWidget />
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="navbar navbar-expand-lg bg-light fixed-top w-100 px-3">
+            <div className="container-fluid">
+                <a className="navbar-brand fs-1" href="#">
+                    La Cueva
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div
+                    className="collapse navbar-collapse justify-content-end fw-bold"
+                    id="navbarNav"
+                >
+                    <ul className="navbar-nav me-4">
+                        <li className="nav-item">
+                            <a
+                                className="nav-link active"
+                                aria-current="page"
+                                href="#"
+                            >
+                                Inicio
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                Armas
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                Armaduras
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                Herramientas
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <CartWidget />
+            </div>
+        </nav>
+    );
 };
 
 export default NavBar;
